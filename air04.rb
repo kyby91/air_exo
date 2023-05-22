14a8890c3a34c7446e061c43dcf9c4da288d59c6
kyby91 <julienmoullec@yahoo.fr>
2023-05-22 17:35:40 +0200

    end air04

---
+++ air04.rb
@@ -0,0 +1,25 @@
+#Find intruder
+
+def check_number_of_arguments(arguments)
+    if arguments.size < 3
+        puts "Wrong number of arguments"
+        return false
+    end
+    
+    return true
+end
+
+def puts_the_intruders(array)
+    i = 0
+    sorted_array = array.sort
+    while i < array.length
+        if sorted_array[i] != sorted_array[i+1]
+            puts sorted_array[i]
+            i += 1
+        else
+            i += 2
+        end
+    end
+end
+
+check_number_of_arguments(ARGV) ? puts_the_intruders(ARGV) : exit(1)
