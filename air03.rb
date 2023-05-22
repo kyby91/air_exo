9dd43f8169de8882ed8196b64d93cb124e6c50f7
kyby91 <julienmoullec@yahoo.fr>
2023-05-22 17:12:05 +0200

    end air03

---
+++ air03.rb
@@ -0,0 +1,29 @@
+#concat
+
+def check_number_of_arguments(arguments)
+    if arguments.size < 3
+        puts "Wrong number of arguments"
+        return false
+    end
+    
+    return true
+end
+
+def give_the_array_and_the_separator(array)
+    last_argument = array[array.size - 1]
+    array.pop()
+    array_of_arguments = array
+    concat_array_of_strings(array_of_arguments, last_argument)
+end
+
+def concat_array_of_strings(array_of_strings, separator)
+    i = 0
+    sentence = ""
+    while i < array_of_strings.size
+        sentence = sentence + array_of_strings[i] + separator
+        i += 1
+    end
+    puts sentence
+end
+
+check_number_of_arguments(ARGV) ? give_the_array_and_the_separator(ARGV) : exit(1)
